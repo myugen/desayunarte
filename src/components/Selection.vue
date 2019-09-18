b3e4d147cab5afdd11fd1de22febe0c6c5c83607
Miguel Cabrera <miguelcabsan@gmail.com>
2019-09-19 00:31:58 +0100

    NONE - Se añade la selección de usuarios

--- src/components/Selection.vue
+++ src/components/Selection.vue
@@ -1,12 +1,27 @@
 <template>
   <v-container>
+    <div class="d-flex flex-column">
+      <div v-for="user in users" :key="user.alias">
+        <v-switch
+          hide-details
+          v-model="user.goOut"
+          :label="user.name"
+        ></v-switch>
+      </div>
+    </div>
+    <v-btn v-on:click="onDrawClick" color="primary">
+      <v-icon small left>fas fa-random</v-icon>
+      <span>Crear grupos</span>
+    </v-btn>
     <v-btn
-      color="blue darken-4"
-      title="Copiar"
-      v-clipboard:copy="users"
+      color="success"
+      v-clipboard:copy="JSON.stringify(users)"
       v-clipboard:success="onCopy"
       v-clipboard:error="onError"
-    ></v-btn>
+    >
+      <v-icon small left>far fa-clipboard</v-icon>
+      <span>Copiar</span>
+    </v-btn>
   </v-container>
 </template>
 <script lang="ts">
@@ -15,18 +30,22 @@ import { User } from "@/model";
 
 export default Vue.extend({
   mounted: function() {
-    console.log(this.users);
+    this.users = this.loadedUsers;
   },
+  data: () => ({
+    users: [] as User[]
+  }),
   methods: {
     onCopy: (e: any) => {
       console.log("Copied!", e.text);
     },
     onError: (e: any) => {
       console.error("An error occurs", e);
-    }
+    },
+    onDrawClick: (e: any) => console.log("Draw!")
   },
   computed: {
-    users: (): User[] => require("@/data/users.json").users as User[],
+    loadedUsers: (): User[] => require("@/data/users.json").users as User[],
     defaultUsersNotJoinToLunch: (): string[] => [
       "rdiaada",
       "msansim",
