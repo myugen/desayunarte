0c4e97132de242f45b1cbfb2c229117fe5eee43e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-29 23:10:10 +0000

    Deploy Myugen/desayunarte to github.com/Myugen/desayunarte.git:gh-pages

---
+++ precache-manifest.115c655c603bf07d0add5bf676fca59b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a72d1486c398a310fa1a",
+    "url": "/desayunarte/css/app.776846db.css"
+  },
+  {
+    "revision": "a72d1486c398a310fa1a",
+    "url": "/desayunarte/js/app.3960827e.js"
+  },
+  {
+    "revision": "e7610a6a28b503971768",
+    "url": "/desayunarte/js/chunk-vendors.1852365d.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/desayunarte/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/desayunarte/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "820d3143b53a8b1e67db4b264090d29e",
+    "url": "/desayunarte/img/logo.820d3143.png"
+  },
+  {
+    "revision": "3d33968f57a4e8eb34d01b3e20ef95cb",
+    "url": "/desayunarte/index.html"
+  },
+  {
+    "revision": "653d077300a12f09a69caeea7a8947f8",
+    "url": "/desayunarte/browserconfig.xml"
+  },
+  {
+    "revision": "0a99ee74b1a7ef4a31e7fa3d5c838703",
+    "url": "/desayunarte/ms-icon-144x144.png"
+  },
+  {
+    "revision": "aed6b90d01d22eff51ab75ed5215684a",
+    "url": "/desayunarte/ms-icon-150x150.png"
+  },
+  {
+    "revision": "8bc6cafde87cef11ba4bc81e0782a038",
+    "url": "/desayunarte/ms-icon-310x310.png"
+  },
+  {
+    "revision": "1dc65b1ec01c9cfd5b8e603f4997362c",
+    "url": "/desayunarte/ms-icon-70x70.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/desayunarte/robots.txt"
+  }
+];
